18dc4bd5092ed787d4cfc34777d01edb0b59a93a
mahinite <arifeenmahin2008@gmail.com>
2026-06-20 21:21:46 +0600

    test

--- src/components/timer/TimerDisplay.tsx
+++ src/components/timer/TimerDisplay.tsx
@@ -6,7 +6,7 @@ interface TimerDisplayProps {
 
 export function TimerDisplay({ time }: TimerDisplayProps) {
   return (
-    <div className="text-[6.5rem] min-[420px]:text-[7.5rem] sm:text-[8.5rem] md:text-[10rem] lg:text-[11.5rem] font-bold tracking-tight my-4 sm:my-6 font-mono leading-none select-none">
+    <div className="text-[7rem] min-[420px]:text-[7.8rem] sm:text-[8.9rem] md:text-[10rem] lg:text-[11.5rem] font-bold tracking-tight my-4 sm:my-6 font-mono leading-none select-none">
       {time}
     </div>
   );
